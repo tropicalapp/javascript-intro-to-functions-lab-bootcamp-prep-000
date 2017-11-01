189c72b3a677c9164a92bd30d7123409974f357c
Cailee Adamson <caileei@gmail.com>
2017-11-01 16:35:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function logWhisper(string) {
   return string.toLowerCase()
 }
 function sayHiToGrandma(string) {
-  return string.toLowerCase()
+  return string.toLowerCase("I can't hear you!")
 }
 function sayHiToGrandma(string) {
   return string.toUpperCase("I can't hear you!")
